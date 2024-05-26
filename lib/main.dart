828928966e9b79b19011e70da8c9c662d19aaf3f
Michael <76718092+MichaelSetia@users.noreply.github.com>
2024-05-26 20:29:50 +0700

    Update Btn

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,64 @@
 import 'package:flutter/material.dart';
-
+ 
 void main() => runApp(MyApp());
-
+ 
 class MyApp extends StatelessWidget {
- @override
- Widget build(BuildContext context) {
-   return MaterialApp(
-     title: 'Flutter Demo',
-     theme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: Colors.blueGrey
-     ),
-     home: Scaffold(
-       appBar: AppBar(
-         title: Text('INI BAR ATASs'),
-       ),
-       body: Center(
-         child: Text('INI ISI'),
-       ),
-     ),
-   );
- }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: BiggerText(text:"Hello world!"), // Ubah widget Heading ke PerubahanText
+        ),
+      ),
+    );
+  }
+}
+ 
+class Heading extends StatelessWidget {
+  final String text;
+ 
+  const Heading({Key? key, required this.text}) : super(key: key);
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
+ 
+class BiggerText extends StatefulWidget {
+  final String text;
+  const BiggerText({Key? key, required this.text}) : super(key: key);
+  @override
+  _BiggerTextState createState() => _BiggerTextState();
+}
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+          child: const Text("Perbesar"),
+          onPressed: () {
+            setState(() {
+              _textSize = 40.0;
+            });
+          },
+        )
+      ],
+    );
+  }
 }
